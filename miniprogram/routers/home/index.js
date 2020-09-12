72b9c95506a008cda26c4dc71e85955120c9e447
wangxi <wangxi@akulaku.com>
2020-09-12 17:41:37 +0800

    vant 组件库 使用 demo

---
+++ miniprogram/routers/home/index.js
@@ -0,0 +1,33 @@
+//index.js
+const app = getApp()
+const pageNo = 1
+const size = 30
+Page({
+  data: {
+    pageNo,
+    size,
+    isHave: true
+  },
+  onReachBottom() {
+    let { isHave, pageNo, size } = this.data
+    if (isHave) {
+      this.getData(pageNo + 1, size)
+    }
+    console.log()
+  },
+  getData(pageNo, size) {
+    //获取数据
+    wx.showLoading({
+      title: '加载中',
+    })
+    setTimeout(() => {
+      console.log(pageNo, size)
+      wx.hideLoading()
+      wx.stopPullDownRefresh()
+    }, 2000);
+  },
+  onPullDownRefresh() {
+    this.getData(pageNo, size)
+  }
+
+})
